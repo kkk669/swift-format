52f12a185d26b5646929459735eb2b1029560367
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-04-05 20:51:49 +0900

    build: follow the change of my username

--- Package.swift
+++ Package.swift
@@ -166,7 +166,7 @@ var dependencies: [Package.Dependency] {
   } else {
     return [
       .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
-      .package(url: "https://github.com/kkk669/swift-markdown.git", branch: "wasm32-wasi"),
+      .package(url: "https://github.com/kkebo/swift-markdown.git", branch: "wasm32-wasi"),
       .package(url: "https://github.com/apple/swift-syntax.git", branch: "main"),
     ]
   }
